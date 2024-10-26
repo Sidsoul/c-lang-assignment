e84d14ee9151066c7dc5ab032b8f473509396faf
Siddhesh Sakhare <siddheshsakhare8@gmail.com>
2024-10-26 21:49:09 +0530

    Update logical.c

--- logical.c
+++ logical.c
@@ -1,1 +1,18 @@
+// program of logical operators
+#include <stdio.h>
+
+int main()
+{
+
+    int a = 20, b = 4;
+
+    // using and
+    printf("a && b : %d\n", a && b);
+   // using or
+    printf("a || b : %d\n", a || b);
+   // using not
+    printf("!a: %d\n", !a);
+
+    return 0;
+}
 
