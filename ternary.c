d6c1384ac04691b7c6b53c38116ae122b8c315ba
Siddhesh Sakhare <siddheshsakhare8@gmail.com>
2024-10-26 22:14:38 +0530

    Update ternary.c

--- ternary.c
+++ ternary.c
@@ -1,7 +1,9 @@
+// Program of ternary opeartor
 #include <stdio.h>
 int main(){
     int a = 10;
     int b = 20;
+    // conditional
     int max = (a>b)?a:b;
     printf("The maximum value is : %d",max);
     return 0;
