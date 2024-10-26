1bada09d545ac73982ef466cc6aadbc194e3cae7
Sidsoul <siddheshsakhare8@gmail.com>
2024-10-26 16:12:07 +0530

    Update

---
+++ assignment.c
@@ -0,0 +1,29 @@
+// Program of Assignment operators
+
+#include <stdio.h>
+
+int main()
+{
+
+    
+    int a = 100;
+    printf("Value of a is %d\n", a);
+
+    
+    a += 10;
+    printf("Value of a is %d\n", a);
+
+    
+    a -= 10;
+    printf("Value of a is %d\n", a);
+
+    
+    a *= 10;
+    printf("Value of a is %d\n", a);
+
+    
+    a /= 10;
+    printf("Value of a is %d\n", a);
+
+    return 0;
+}
