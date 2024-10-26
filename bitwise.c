6305a2f65d91a82b970c66fd63a825761bad45ba
Siddhesh Sakhare <siddheshsakhare8@gmail.com>
2024-10-26 21:30:38 +0530

    Update bitwise.c

--- bitwise.c
+++ bitwise.c
@@ -4,26 +4,12 @@
 int main()
 {
     int x = 3, y = 8;
-
-    
     printf("x = %d, y = %d\n", x, y);
     printf("x&y = %d\n", x & y);
-
-    
     printf("x|y = %d\n", x | y);
-
-    
     printf("x^y = %d\n", x ^ y);
-
-    
-    
     printf("~x = %d\n", x = ~x);
-
-    
     printf("y<<1 = %d\n", y << 1);
-
-    
     printf("y>>1 = %d\n", y >> 1);
-
     return 0;
 }
