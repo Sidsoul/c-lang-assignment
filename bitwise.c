a9a76ed706cd6484eca49e48dde3a26170ace012
Sidsoul <siddheshsakhare8@gmail.com>
2024-10-26 16:02:40 +0530

    Update

---
+++ bitwise.c
@@ -0,0 +1,29 @@
+// Program of bitwise operators
+
+#include <stdio.h>
+int main()
+{
+    int x = 3, y = 8;
+
+    
+    printf("x = %d, y = %d\n", x, y);
+    printf("x&y = %d\n", x & y);
+
+    
+    printf("x|y = %d\n", x | y);
+
+    
+    printf("x^y = %d\n", x ^ y);
+
+    
+    
+    printf("~x = %d\n", x = ~x);
+
+    
+    printf("y<<1 = %d\n", y << 1);
+
+    
+    printf("y>>1 = %d\n", y >> 1);
+
+    return 0;
+}
