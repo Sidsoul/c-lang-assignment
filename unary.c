4c6616b11ce3fd168d53008a86486aea11e85bc9
Siddhesh Sakhare <siddheshsakhare8@gmail.com>
2024-10-26 22:04:51 +0530

    Update unary.c

--- unary.c
+++ unary.c
@@ -1,1 +1,11 @@
+// Program of unary operators
+#include <stdio.h>
+int main(){
 
+  // using increment
+  int a = 10;
+  printf("a++ %d",a++);
+  // using decrement
+  printf("a-- %d",a--);
+  return 0;
+}
