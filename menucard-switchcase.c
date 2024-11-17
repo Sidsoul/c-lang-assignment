b15f049d1e071e770740ff07a6ba97633b469f07
Siddhesh Sakhare <siddheshsakhare8@gmail.com>
2024-11-17 10:29:44 +0530

    Update menucard-switchcase.c

--- menucard-switchcase.c
+++ menucard-switchcase.c
@@ -2,10 +2,11 @@
 int main(){
     int a;
     int ans = 1;
+    int total = 0;
     while(ans==1){
     printf("Menu :\n");
-    printf("No. 1 : Burger - Rs. 500\n ");
-    printf("No. 2 : Pizza - Rs. 600\n ");
+    printf("No. 1 : Burger - Rs. 50\n");
+    printf("No. 2 : Pizza - Rs. 60\n");
     printf("Order number : ");
     scanf("%d",&a);
     int b,c,x,y;
@@ -13,23 +14,28 @@ int main(){
         case 1:
         printf("How much pieces ? : ");
         scanf("%d",&b);
-        c = b*500;
+        c = b*50;
         printf("Your burgers order's total amount is Rs. %d\n",c);
         break;
         case 2:
         printf("How much pieces ? : ");
         scanf("%d",&x);
-        y = x*600;
+        y = x*60;
         printf("Your pizzas order's total amount is Rs. %d\n",y);
         break;
         default:
         printf("item not available");
         break;
     }
-    printf("Do you want to add more?....(1/0)");
+    total += c+y;
+    printf("Your total bill is : %d\n",total);
+    c = 0;
+    y = 0;
+    printf("Do you want to add more?....yes(1)/no(0)");
     scanf("%d",&ans);
-
-
+    if(ans!=1){
+        printf("Thank you and pls visit again...");
+    }
     }
     return 0;
 }
