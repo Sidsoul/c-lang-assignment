30d14369b1a1094aa561a6cae7b22a201fffdc25
Siddhesh Sakhare <siddheshsakhare8@gmail.com>
2024-12-04 09:02:01 +0530

    Create tableofno.c

---
+++ tableofno.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(){
+    int a;
+    printf("Enter a number : ");
+    scanf("%d",&a);
+    for(int i = 1; i < 11; i++){
+        printf("%d x %d = %d\n",a,i,a*i);
+    }
+    return 0;
+}
