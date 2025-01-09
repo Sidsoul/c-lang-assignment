78c210fe365108476ed2ccd79c3852d5c2123e35
Siddhesh Sakhare <siddheshsakhare8@gmail.com>
2025-01-09 03:43:13 -0800

    Create voting.c

---
+++ voting.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(){
+    int a;
+    printf("Enter your age : ");
+    scanf("%d",&a);
+    if(a<18){
+        printf("You are not eligible to vote");
+    }
+    else{
+        printf("You are eligible to vote ");
+    }
+    return 0;
+}
